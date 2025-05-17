222a029bcd2a480d4a2e7fa4f4976caa93eeb3c1
Jonno <Jonathan.s.bourne@gmail.com>
2025-05-17 14:53:43 +0100

    Improved the full parse script and added requirements doc

--- full_ocod_parse_process.py
+++ full_ocod_parse_process.py
@@ -6,89 +6,119 @@ from locate_and_classify_helper_functions import (load_postocde_district_lookup,
 import re
 import zipfile
 import sys
+from typing import List
 
 """
-This script runs the full parsing pipeline.
-It takes a single argument which is the root path of the empty_homes_data path folder.
-It requires that all data is found in that folder.
-I hope to update the script so that it is easier to swap out the data for new data when it is created
+Enhanced OCOD (Overseas Companies Ownership Data) Parser
 
+This script processes raw OCOD data through a comprehensive pipeline that creates a structured dataset and
+enriches property addresses with additional metadata, classifies properties, and identifies 
+business/residential usage.
 
-docker run --rm -it -v $(pwd):/app jonno/parse_process:test ./app/enhance_ocod/full_ocod_parse_process.py ./app/data Malcom_UK_Owners_Missing.csv Malcom_UK_Owners_Missing_enhanced.csv
+Pipeline Steps:
+1. Address Parsing: Uses Spacy NER to extract structured components from property addresses
+2. Data Enhancement: Enriches addresses with:
+   - Postcode data (via ONSPD)
+   - Price paid information (from Land Registry)
+   - Business ratings (from VOA)
+3. Classification: Determines property types and identifies multi-unit properties
+
+Required Data Files (must be in root_path):
+- Input CSV file: Raw OCOD data with property addresses
+- spacy_cpu_model/: Trained Spacy model for address component recognition
+- ONSPD.zip: ONS Postcode Directory containing postcode to area mappings
+- VOA_ratings.csv: VOA business ratings list
+- price_paid_files/: Directory containing Land Registry price paid data
+
+Usage:
+    python full_ocod_parse_process.py <root_path> <input_file> <output_file>
+
+Arguments:
+    root_path: Path to directory containing all required data files
+    input_file: Name of input CSV file in root_path
+    output_file: Name for enhanced output CSV file
+
+Example:
+    python full_ocod_parse_process.py ./data/ input.csv output_enhanced.csv
+
+Docker Usage:
+    docker run --rm -it -v $(pwd):/app jonno/parse_process:test \
+        ./app/enhance_ocod/full_ocod_parse_process.py \
+        ./app/data input.csv output.csv
+
 """
 
-args = sys.argv  
+def main(args: List[str]) -> None:
+    root_path = str(args[1])
+    data_file = str(args[2])
+    output_file = str(args[3])
+    # Load and prepare initial data
+    ocod_data = load_and_prep_OCOD_data(root_path + data_file)
 
-root_path = str(args[1])
+    # NLP processing
+    all_entities = spacy_pred_fn(spacy_model_path=root_path+'spacy_cpu_model', ocod_data=ocod_data)
+    full_expanded_data = parsing_and_expansion_process(all_entities, expand_addresses=True)
+    del all_entities  # memory management
+    
+    ocod_data = post_process_expanded_data(full_expanded_data, ocod_data)
+    del full_expanded_data  # memory management
 
-data_file = str(args[2])
+    # Load and process ONSPD data
+    print("Load ONSPD")
+    zip_file = zipfile.ZipFile(root_path + 'ONSPD.zip')
+    target_zipped_file = [i for i in zip_file.namelist() if re.search(r'^Data/ONSPD.+csv$',i)][0]
+    postcode_district_lookup = load_postocde_district_lookup(root_path + "ONSPD.zip", target_zipped_file)
 
-ouput_file = str(args[3])
+    # Pre-process and enhance data
+    print("Pre-process expanded ocod data")
+    ocod_data = preprocess_expandaded_ocod_data(ocod_data, postcode_district_lookup)
+    
+    print("Load and pre-process the Land Registry price paid dataset")
+    price_paid_df = load_and_process_pricepaid_data(root_path+'price_paid_files/', postcode_district_lookup)
+    
+    print("Add in missing Local authority codes to the ocoda dataset")
+    ocod_data = add_missing_lads_ocod(ocod_data, price_paid_df)
+    
+    print("Load and pre-process the voa business ratings list dataset")
+    voa_businesses = load_voa_ratinglist(root_path +'VOA_ratings.csv', postcode_district_lookup)
+    del postcode_district_lookup  # memory management
 
+    # Address matching
+    print("Match street addresses and buildings")
+    ocod_data = street_and_building_matching(ocod_data, price_paid_df, voa_businesses)
 
-ocod_data = load_and_prep_OCOD_data(root_path + data_file)
+    print('Sub-street matching, this takes some time')
+    ocod_data = substreet_matching(ocod_data, price_paid_df, voa_businesses)
+    del price_paid_df  # memory management
 
-all_entities = spacy_pred_fn(spacy_model_path = root_path+'spacy_cpu_model', ocod_data = ocod_data)
-#all_entities = load_cleaned_labels(root_path + 'full_dataset_no_overlaps.json')
-full_expanded_data = parsing_and_expansion_process(all_entities, expand_addresses = True)
+    # Business processing
+    print('Add in businesses per oa and lsoa')
+    ocod_data = counts_of_businesses_per_oa_lsoa(ocod_data, voa_businesses)
 
-del all_entities #memory management
+    print('Identify businesses using address matching')
+    ocod_data = voa_address_match_all_data(ocod_data, voa_businesses)
+    del voa_businesses  # memory management
 
-ocod_data = post_process_expanded_data(full_expanded_data, ocod_data)
+    # Classification
+    print('Classification type 1')
+    ocod_data = classification_type1(ocod_data)
+    print('Classification type 2')
+    ocod_data = classification_type2(ocod_data)
 
-del full_expanded_data #memory management
+    print('Contract ocod dataset')
+    ocod_data = contract_ocod_after_classification(ocod_data, class_type='class2', classes=['residential'])
 
-print("Load ONSPD")
-# zip file handler  
-zip = zipfile.ZipFile(root_path + 'ONSPD.zip')
-# looks in the data folder for a csv file that begins ONSPD
-#This will obviously break if the ONS change the archive structure
-target_zipped_file = [i for i in zip.namelist() if re.search(r'^Data\/ONSPD.+csv$',i) ][0]
-postcode_district_lookup = load_postocde_district_lookup(root_path + "ONSPD.zip", target_zipped_file)
+    # Save results
+    print('Process complete saving the enhanced ocod dataset to ' + root_path + output_file)
+    columns = ['title_number', 'within_title_id', 'within_larger_title', 'unique_id', 
+              'unit_id', 'unit_type', 'building_name', 'street_number', 'street_name', 
+              'postcode', 'city', 'district', 'region', 'property_address', 'oa11cd', 
+              'lsoa11cd', 'msoa11cd', 'lad11cd', 'class', 'class2']
+              
+    ocod_data.loc[:, columns].rename(columns={
+        'within_title_id': 'nested_id',
+        'within_larger_title': 'nested_title'
+    }).to_csv(root_path + output_file)
 
-print("Pre-process expanded ocod data")
-ocod_data = preprocess_expandaded_ocod_data(ocod_data, postcode_district_lookup)
-print("Load and pre-process the Land Registry price paid dataset")
-#loads from a folder of price paid files
-price_paid_df = load_and_process_pricepaid_data(root_path+'price_paid_files/', postcode_district_lookup)
-print("Add in missing Local authority codes to the ocoda dataset")
-ocod_data = add_missing_lads_ocod(ocod_data, price_paid_df)
-print("Load and pre-process the voa business ratings list dataset")
-voa_businesses = load_voa_ratinglist(root_path +'VOA_ratings.csv', postcode_district_lookup)
-
-del postcode_district_lookup #for memory purposes
-
-print("Match street addresses and buildings")
-ocod_data = street_and_building_matching(ocod_data, price_paid_df, voa_businesses)
-
-#This takes some time
-print('Sub-street matching, this takes some time')
-ocod_data = substreet_matching(ocod_data, price_paid_df, voa_businesses)
-
-del price_paid_df #for memory purposes
-print('Add in businesses per oa and lsoa')
-ocod_data = counts_of_businesses_per_oa_lsoa(ocod_data, voa_businesses)
-
-print('Identify businesses using address matching')
-ocod_data = voa_address_match_all_data(ocod_data, voa_businesses)
-
-del voa_businesses #probably not necessary but still delete to save memory
-
-print('Classification type 1')
-ocod_data = classification_type1(ocod_data)
-print('Classification type 2')
-ocod_data = classification_type2(ocod_data)
-
-print('Contract ocod dataset')
-ocod_data = contract_ocod_after_classification(ocod_data, class_type = 'class2', classes = ['residential'] )
-
-print('Process complete saving the enchanced ocod dataset to ' + root_path + ouput_file)
-
-#subset the dataframe to only the columns necessary for the dataset and save
-ocod_data.loc[:, ['title_number', 'within_title_id', 'within_larger_title', 'unique_id', 'unit_id', 'unit_type',
-       'building_name', 'street_number', 'street_name', 'postcode', 'city',
-       'district',  'region', 'property_address', 'oa11cd', 'lsoa11cd',
-       'msoa11cd',  'lad11cd', 'class', 'class2']].rename(columns={'within_title_id':'nested_id',
-                                                                  'within_larger_title':'nested_title'}).to_csv(root_path + ouput_file)
-
-#FINISH!
+if __name__ == '__main__':
+    main(sys.argv)
