12808440464fd747e1703c7dbc07aae3182f88c4
Jonno <Jonathan.s.bourne@gmail.com>
2025-09-19 07:45:16 +0100

    update tomol and init

--- src/enhance_ocod/__init__.py
+++ src/enhance_ocod/__init__.py
@@ -2,4 +2,10 @@
 A package for enhancing and processing OCOD (Overseas companies that own property in England and Wales ) dataset
 """
 
-__version__ = "0.1.0"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("enhance-ocod")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "0.0.0"
